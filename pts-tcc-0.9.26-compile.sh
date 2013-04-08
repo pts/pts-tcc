c687b84c65a34a85235b022c78fed8f86372ee8c
Peter Szabo <pts@fazekas.hu>
2013-04-08 14:53:59 +0000

    fixed gcc errors; added support for tcc-0.9.26

--- pts-tcc-0.9.26-compile.sh
+++ pts-tcc-0.9.26-compile.sh
@@ -1,10 +1,10 @@
 #! /bin/bash --
 #
 # Build pts-tcc using uClibc.
-# by pts@fazekas.hu at Sun Nov  1 12:12:16 CET 2009
+# by pts@fazekas.hu at Mon Apr  8 15:45:02 CEST 2013
 #
 # To create the patch:
-# for F in tcc-0.9.25/*.{c,h}.orig; do diff -u -U10 "$F" "${F%.orig}"; done >tccb/pts-tcc-0.9.25.patch
+# for F in tcc-0.9.26/{*.{c,h},configure}.orig; do diff -u -U10 "$F" "${F%.orig}"; done >pts-tcc-0.9.26.patch
 # Using uClibc 0.9.30.1.
 #
 
@@ -22,20 +22,20 @@ type -p gcc
 type -p ld
 type -p perl
 
-test -f tcc-0.9.25.tar.bz2 || wget http://download.savannah.nongnu.org/releases/tinycc/tcc-0.9.25.tar.bz2
-test -f tcc-0.9.25.tar.bz2
-test -f pts-tcc-0.9.25.patch || wget http://pts-mini-gpl.googlecode.com/svn/trunk/pts-tcc/pts-tcc-0.9.25.patch
-test pts-tcc-0.9.25.patch
+test -f tcc-0.9.26.tar.bz2 || wget http://download.savannah.nongnu.org/releases/tinycc/tcc-0.9.26.tar.bz2
+test -f tcc-0.9.26.tar.bz2
+test -f pts-tcc-0.9.26.patch || wget http://pts-mini-gpl.googlecode.com/svn/trunk/pts-tcc/pts-tcc-0.9.26.patch
+test pts-tcc-0.9.26.patch
 
 bash ./i386-uclibc-gcc.c  # create i386-uclibc-gcc
 CC="$PWD/i386-uclibc-gcc"
-rm -rf tcc-0.9.25
-tar xjf tcc-0.9.25.tar.bz2
-patch -p0 <pts-tcc-0.9.25.patch
+rm -rf tcc-0.9.26
+tar xjf tcc-0.9.26.tar.bz2
+(cd tcc-0.9.26 && patch -p1 <../pts-tcc-0.9.26.patch) || exit "$?"
 
-(cd tcc-0.9.25 && ./configure --cc="$CC -static" --extra-cflags='-DCONFIG_TCC_STATIC') || exit "?"
-echo "CONFIG_NOLDL=yes" >>tcc-0.9.25/config.mak
-(cd tcc-0.9.25 && make libtcc1.a) || exit "$?"
+(cd tcc-0.9.26 && ./configure --cc="$CC -static" --extra-cflags='-DCONFIG_TCC_STATIC') || exit "?"
+echo "CONFIG_NOLDL=yes" >>tcc-0.9.26/config.mak
+(cd tcc-0.9.26 && make libtcc1.a) || exit "$?"
 
 rm -rf tcclibc
 mkdir tcclibc
@@ -50,7 +50,7 @@ for F in "$UCLIBC_USR"/lib/lib{c,m,crypt,util}.a; do
   ls -l "$F"
   (cd tcclibc && ar x "$F") || exit "$?"
 done
-(cd tcclibc && ar x ../tcc-0.9.25/libtcc1.a) || exit "$?"
+(cd tcclibc && ar x ../tcc-0.9.26/libtcc1.a) || exit "$?"
 rm -f tcclibc.a  # ar cr doesn't remove the file.
 (cd tcclibc && ar cr ../tcclibc.a *.o)
 ls -l tcclibc.a
@@ -58,10 +58,9 @@ for F in tcclibc.a "$UCLIBC_USR"/lib/{crt1.o,crti.o,crtn.o}; do
   G="${F##*/}"
   export NAME="data_${G%.*}"
   perl -e '$_=join("",<STDIN>); my$L=length; s@([^-+/\w])@sprintf"\\%03o",ord$1@ge; print".globl $ENV{NAME}\n.section .data\n.align 4\n.size $ENV{NAME},$L\n$ENV{NAME}:\n.string \"$_\\001\"\n"' <"$F"
-done >tcc-0.9.25/libcdata.s
+done >tcc-0.9.26/libcdata.s
 
-# make tcc does: /tmp/tccb/i386-uclibc-gcc -static -o tcc tcc.c -DTCC_TARGET_I386 -DCONFIG_TCC_STATIC -g -Wall -fno-strict-aliasing -mpreferred-stack-boundary=2 -march=i386 -falign-functions=0 -Wno-pointer-sign -Wno-sign-compare -D_FORTIFY_SOURCE=0 -lm
-(cd tcc-0.9.25 && $CC -static -o ../pts-tcc.uncompressed tcc.c libcdata.s -DTCC_TARGET_I386 -O2 -s -Wall -fno-strict-aliasing -mpreferred-stack-boundary=2 -march=i386 -falign-functions=0 -Wno-pointer-sign -Wno-sign-compare -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0 -lm -DCONFIG_TCC_STATIC -Wno-unused-but-set-variable -Wno-array-bounds) || exit "$?"
+(cd tcc-0.9.26 && $CC -static -o ../pts-tcc.uncompressed tcc.c libtcc.c tccelf.c tccgen.c tccpp.c i386-gen.c tccrun.c tccasm.c i386-asm.c libcdata.s -DTCC_TARGET_I386 -O2 -s -Wall -fno-strict-aliasing -mpreferred-stack-boundary=2 -march=i386 -falign-functions=0 -Wno-pointer-sign -Wno-sign-compare -lm -DCONFIG_TCC_STATIC -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0) || exit "$?"
 ls -l pts-tcc.uncompressed
 cp -f pts-tcc.uncompressed pts-tcc
 upx.pts --best pts-tcc || upx --best pts-tcc
