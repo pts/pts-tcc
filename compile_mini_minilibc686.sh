c2e8879d31e5e0b794a2209e0997a72fc7798102
Peter Szabo <pts@fazekas.hu>
2023-06-26 03:33:43 +0200

    finised the minilibc686 port (compile_mini_minilibc686.sh), it works now

--- compile_mini_minilibc686.sh
+++ compile_mini_minilibc686.sh
@@ -1,13 +1,13 @@
 #! /bin/sh --
 set -ex
 
-../pathbin/minicc --gcc -DCONFIG_TCCDIR='"/dev/null"' -DTCC_VERSION='"0.9.26-1"' -DTCC_VERSION10000=92600 -DTCC_TARGET_I386 -DCONFIG_TCC_STATIC -DCONFIG_NO_RUN -DCONFIG_NO_EXEC -fno-strict-aliasing \
+# --wcc generates the smallest compressed code, but its `long double' is just 64-bit.
+# --gcc=4.8 generates the smallest uncompressed code.
+# Compiles with: --gcc -gcc=clang --gcc=4.4 --tcc --pcc --wcc.
+../pathbin/minicc --gcc=4.8 -DCONFIG_TCCDIR='"/dev/null"' -DTCC_VERSION='"0.9.26-1"' -DTCC_VERSION10000=92600 -DTCC_TARGET_I386 -DCONFIG_TCC_STATIC -DCONFIG_NO_RUN -DCONFIG_NO_EXEC -fno-strict-aliasing \
     -Wno-unused-parameter -Wno-sign-compare -Wno-missing-field-initializers -Wno-pointer-sign -Wno-unused-result -Wno-shift-negative-value \
     -o pts-tcc-minil.unc \
     tcc-0.9.26/tcc.c tcc-0.9.26/libtcc.c tcc-0.9.26/tccelf.c tcc-0.9.26/tccgen.c tcc-0.9.26/tccpp.c tcc-0.9.26/i386-gen.c tcc-0.9.26/tccasm.c tcc-0.9.26/i386-asm.c
-# TODO(pts): Currently it doesn't link, there are undefined symbols.
-# TODO(pts): Can we replace strtof with strtod?
-# TODO(pts): Undefined symbols: malloc+realloc+free strtof strtold strtoll strtoul strtoull
 ls -ld pts-tcc-minil.unc
 dl/upx --ultra-brute --no-lzma -q -q -f -o pts-tcc-minil.ubr pts-tcc-minil.unc
 ls -ld pts-tcc-minil.unc pts-tcc-minil.ubr
